46b7f897fd3d4803a454807bf63086a369c96529
Kari Sabine Malmin <kari.malmin@makingwaves.com>
2015-09-14 14:35:27 +0200

    Added watch task and browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,30 @@
+/////modules
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
 
-gulp.task('default', function() {
-	console.log('Hei!');
-});
-
+/////tasks
 gulp.task('sass', function () {
-  return gulp.src('app/scss/styles.scss') // Get source files with gulp.src
+  return gulp.src('app/scss/*.scss') // Get source files with gulp.src
     .pipe(sass()) // Sends it through a gulp plugin
     .pipe(gulp.dest('app/css')) // Outputs the file in the destination folder
-})
+    .pipe(browserSync.reload({
+    	stream: true
+    }))
+});
+
+gulp.task('browserSync', function() {
+	browserSync({
+		server: {
+			baseDir: '.'
+		},
+	})
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function () {
+	gulp.watch('app/scss/*.scss', ['sass']);	
+});
+
+gulp.task('default', ['watch'], function () {
+});
