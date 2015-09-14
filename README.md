86bacd6a2d299775ad8859e4970c11a36ab73cfe
Kari Sabine Malmin <kari.malmin@makingwaves.com>
2015-09-14 14:50:41 +0200

    Misc

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # gulp-test
 
-Run command 'npm install' to download node_modules folder
+<h2>node_modules</h2>
+Run command 'npm install' to download node_modules folder.
+
+Based on the article <a href="https://css-tricks.com/gulp-for-beginners/">Gulp for beginners @ CSS-tricks</a>.
--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
 
 /////tasks
 gulp.task('sass', function () {
@@ -26,5 +28,27 @@ gulp.task('watch', ['browserSync', 'sass'], function () {
 	gulp.watch('app/scss/*.scss', ['sass']);	
 });
 
+gulp.task('useref', function() {
+	var assets = useref.assets();
+
+	return gulp.src('app/*.html')
+	.pipe(assets)
+	.pipe(assets.restore())
+	.pipe(useref())
+	.pipe(gulp.dest('dist'))
+});
+
+gulp.task('useref', function(){
+	var assets = useref.assets();
+
+	return gulp.src('app/*.html')
+		.pipe(assets)
+		.pipe(uglify())
+		.pipe(assets.restore())
+		.pipe(useref())
+		.pipe(gulp.dest('dist'))
+})
+
+///default task
 gulp.task('default', ['watch'], function () {
 });
